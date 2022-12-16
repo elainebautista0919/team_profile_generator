56531c7c561a6131839d785a94eaaf9a551b516f
Elaine Bautista <elainebautista@Elaines-MacBook-Pro.local>
2022-12-16 18:56:13 -0500

    Updated JS with HTML template

--- src/template.js
+++ src/template.js
@@ -49,4 +49,62 @@ const createTeam = (team) => {
                 </ul>
             </div>
         </div>`;
-}
+
+    // Create emply array for employee cards in HTML
+    const html = [];
+
+    // Add manager to HTML cards
+    html.push(team.filter(employee => employee.getRole() === 'Manager').map((manager) => createManager(manager)));
+
+    // Add engineer to HTML cards
+    html.push(team.filter(employee => employee.getRole() === 'Engineer').map((engineer) => createEngineer(engineer)).join(''));
+
+    // Add intern to HTML cards
+    html.push(team.filter(employee => employee.getRole() === 'Intern').map((intern) => createEngineer(intern)).join(''));
+    
+    return html.join('\n');
+};
+
+// Create HTML from template
+const createTemplate = (team) => `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+        crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../dist/style.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="team-profile-header col-12 mb-3 team-profile jumbotron">
+                <h1 class="team-profile-title text-center">Meet the Team</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="team-mem-container col-12 d-flex flex-wrap justify-content-around">
+                ${createTeam(team)}
+            </div>
+        </div>
+    </div>
+    <script src="https://kit.fontawesome.com/cba39ebf54.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
+`;
+
+module.exports = renderMain;
